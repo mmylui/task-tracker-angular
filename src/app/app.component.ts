3240bdb6d05ba920a237a2b4db249942d165e1e2
Mina_Lui <mina.lui@diegesis.co.uk>
2023-12-13 09:58:36 +0000

    task tracker app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, TasksComponent, HttpClientModule, FooterComponent]
 })
 export class AppComponent {
-  title = 'angular-crash';
+
 }
